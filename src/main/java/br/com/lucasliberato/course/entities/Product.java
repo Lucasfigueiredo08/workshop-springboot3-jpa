c11125b87d2fc8294569f9942146eb1f4ca761b4
Lucas Figueiredo <lukas.liberato14@gmail.com>
2024-05-07 19:26:01 -0300

    Many-to-many association with JoinTable

--- src/main/java/br/com/lucasliberato/course/entities/Product.java
+++ src/main/java/br/com/lucasliberato/course/entities/Product.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 
 import java.io.Serializable;
 import java.util.HashSet;
@@ -25,8 +27,10 @@ public class Product implements Serializable {
     private String description;
     private Double price;
     private String imgUrl;
-
-    @Transient
+    @ManyToMany
+    @JoinTable(name = "tb_product_category",
+            joinColumns = @JoinColumn(name = "product_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
 
     public Product() {
@@ -78,6 +82,10 @@ public class Product implements Serializable {
 
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
     }
 
     @Override
