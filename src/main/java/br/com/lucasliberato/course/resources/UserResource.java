a3e94aa1b1b63278b6ada77b207e5dc4f8f81d05
Lucas Figueiredo <lukas.liberato14@gmail.com>
2024-05-12 20:12:43 -0300

    Deleção de User

--- src/main/java/br/com/lucasliberato/course/resources/UserResource.java
+++ src/main/java/br/com/lucasliberato/course/resources/UserResource.java
@@ -4,6 +4,7 @@ import br.com.lucasliberato.course.entities.User;
 import br.com.lucasliberato.course.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,4 +43,12 @@ public class UserResource {
                 .buildAndExpand(user.getId()).toUri();
         return ResponseEntity.created(uri).body(user);
     }
+
+    @DeleteMapping
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
 }
