40dfd480990da9716aaaa249bd11d2f7221287bc
Lucas Figueiredo <lukas.liberato14@gmail.com>
2024-05-14 20:03:44 -0300

    Exception handling - delete

--- src/main/java/br/com/lucasliberato/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/br/com/lucasliberato/course/resources/exceptions/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.lucasliberato.course.resources.exceptions;
 
+import br.com.lucasliberato.course.services.exceptions.DatabaseException;
 import br.com.lucasliberato.course.services.exceptions.ResourceNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
@@ -22,4 +23,15 @@ public class ResourceExceptionHandler {
 
         return ResponseEntity.status(status).body(err);
     }
+
+    @ExceptionHandler(DatabaseException.class)
+    public ResponseEntity<StandardError> database(DatabaseException e, HttpServletRequest request) {
+        String error = "Database error";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+
+        return ResponseEntity.status(status).body(err);
+    }
+
+
 }
